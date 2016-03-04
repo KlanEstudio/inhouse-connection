1218835be28eb8b65cd64629d49b00b9a3b9dec0
Andrés Javier López <ajavier.lopez@gmail.com>
2016-03-04 15:19:19 -0600

    Agrega nuevos campos link y target para menu global
    Agrega reemplazos de variables para los campos link y target para que
    funcionen correctamente.

--- inhouse-connection.php
+++ inhouse-connection.php
@@ -59,26 +59,26 @@ class InhouseAuthException extends Exception {}
 class InhouseContentException extends Exception{}
 
 class InHouse {
-	
+
 	protected $caller;
-	
+
 	protected $media_url = MEDIA_URL;
-	
+
 	protected $valores = array();
-		
+
 	protected $connection = true;
-	
+
 	public function __construct($id_dominio, $token)
 	{
 		$this->caller = new Caller($id_dominio, $token);
 	}
-	
+
 	public function parse_html($contents) {
-        $contents = str_replace("\r\n","\n", $contents);
+		$contents = str_replace("\r\n","\n", $contents);
 		$contents = str_replace("\r","\n", $contents);
 
 		preg_match_all('/{get:(\w*)}/i', $contents, $vars);
-		
+
 		foreach($vars[1] as $var) {
 			$strvar = isset($_GET[$var])?$_GET[$var]:'';
 			$strvar = htmlentities($strvar);
@@ -86,7 +86,7 @@ class InHouse {
 		}
 
 		preg_match_all('/{custom:(\w+(\(\w+\))?)}/i', $contents, $vars);
-		
+
 		foreach($vars[1] as $var) {
 			$custom = explode('(', trim($var, ')'));
 			$name = $custom[0];
@@ -104,40 +104,40 @@ class InHouse {
 				$contents = str_replace('{custom:'.$var.'}', (string) $object, $contents);
 			}
 		}
-		
+
 		preg_match_all('/{(\w+:\w+(\|\w*)?:\w+)}/i', $contents, $vars);
-		
-		foreach($vars[1] as $var) {			
+
+		foreach($vars[1] as $var) {
 			$info = explode(':', $var);
-			
-            try {
-    			$contents = str_replace( '{'.$var.'}', $this->getData($info[0], $info[1], $info[2]), $contents);
-            }
-            catch(InhouseContentException $e) {}
+
+			try {
+				$contents = str_replace( '{'.$var.'}', $this->getData($info[0], $info[1], $info[2]), $contents);
+			}
+			catch(InhouseContentException $e) {}
 		}
-		
+
 		preg_match_all('/<!--#\s?inh:(\w+\([\d+|\w+:\w+:\w+]+(,\d+(\-\w+)?)?\))\s?-->(<[^!]|[^<])*<!--#\s?close:inh\s?-->/i', $contents, $vars, PREG_SET_ORDER );
-		
+
 		foreach($vars as $plantilla) {
 			$contents = str_replace($plantilla[0], $this->processTemplate($plantilla[0], $plantilla[1]), $contents);
 		}
-		
+
 		preg_match_all('/<!--#\s?inh:pag:(\w+\([\d+|\w+:\w+:\w+]+,\d+\))\s?-->(<[^!]|[^<])*<!--#\s?close:inh\s?-->/i', $contents, $vars, PREG_SET_ORDER );
 		foreach($vars as $paginacion) {
 			$contents = str_replace($paginacion[0], $this->processPaginacion($paginacion[0], $paginacion[1]), $contents);
 		}
-			
+
 		return $contents;
 	}
-	
+
 	protected function getData($modulo, $campo, $id) {
 		if($modulo == 'imagen' || $modulo == 'video' || $modulo == 'banner' || $modulo == 'audio' || $modulo == 'galeria_simple' || $modulo == 'imagen_simple' || $modulo == 'video_simple' || $modulo == 'audio_simple') {
 			$modulo = 'media';
 		}
-		
+
 		$adv_campo = explode('|', $campo);
 		$campo = $adv_campo[0];
-		
+
 		$intid = (int) $id;
 		if($intid == 0) {
 			$id = isset($_GET[$id])?$_GET[$id]:NULL;
@@ -145,15 +145,15 @@ class InHouse {
 		if(is_null($id)) {
 			throw new InhouseContentException();
 		}
-		
+
 		if(!isset($this->valores[$modulo][$id])) {
 			$data = $this->caller->get_json($this->caller->construct_call($modulo, $id));
 			if($data->auth == false) {
 				throw new InhouseAuthException();
 			}
-			$this->valores[$modulo][$id] = $data->$modulo;			
+			$this->valores[$modulo][$id] = $data->$modulo;
 		}
-		
+
 		$format_date = true;
 		$formatofecha = DATE_FORMAT;
 		if($campo == 'fecha_raw') {
@@ -164,7 +164,7 @@ class InHouse {
 			$formatofecha = DATE_URL;
 			$campo = 'fecha';
 		}
-		
+
 		if($campo == 'id_galeria' || $campo == 'portada_thumb' || $campo == 'portada_imagen') {
 			$field = $campo;
 		}
@@ -180,46 +180,52 @@ class InHouse {
 		elseif($modulo == 'infogaleria') {
 			$field = $campo.'_galeria';
 		}
+		elseif($campo == 'link') {
+			$field = 'link';
+		}
+		elseif($campo == 'target') {
+			$field = 'target';
+		}
 		else {
 			$field = $campo.'_'.$modulo;
 		}
-										
+
 		$valor = isset($this->valores[$modulo][$id]->$field)?$this->valores[$modulo][$id]->$field:'';
-		
+
 		if($field == 'portada_thumb' || $field == 'portada_imagen' || $field == 'ruta_media' || $field == 'thumb_media') {
 			$valor = $this->media_url.$valor;
 		}
-		
+
 		if($campo == 'fecha' && $format_date) {
 			$valor = format_date($valor, $formatofecha);
 		}
-		
+
 		if($campo == 'audio_mp3') {
 			$valor = $this->media_url.str_replace('.ogg', '.mp3', $valor);
 		}
-		
+
 		if($campo == 'audio_ogg') {
 			$valor = $this->media_url.str_replace('.mp3', '.ogg', $valor);
 		}
-		
+
 		if(isset($adv_campo[1])) {
 			$fn = $adv_campo[1];
 			if(function_exists($fn)) {
 				$valor = $fn($valor);
 			}
 		}
-		
+
 		return $valor;
 	}
-	
+
 	protected function processTemplate($original, $identificador) {
 		list($modulo, $id) = explode('(', trim($identificador, ')'));
-				
+
 		$total = 0;
 		$pag = 1;
 		if(strpos($id, ',') !== false) {
 			$limit = explode(',', $id);
-			$id = $limit[0];				
+			$id = $limit[0];
 			$total = $limit[1];
 			if(strpos($total, '-') !== false) {
 				$tot = explode('-', $total);
@@ -230,10 +236,10 @@ class InHouse {
 				}
 			}
 		}
-				
+
 		if((int) $id == 0) {
 			$conf = explode(':', $id);
-			
+
 			if(count($conf) == 3) {
 				$id = $this->getData($conf[0], $conf[1], $conf[2]);
 			}
@@ -241,11 +247,11 @@ class InHouse {
 				$id = isset($_GET[$id])?$_GET[$id]:0;
 			}
 		}
-				
+
 		if($modulo == 'banners' || $modulo == 'banner' || $modulo == 'videos' || $modulo == 'imagenes' || $modulo == 'audios') {
 			$modulo = 'medias';
 		}
-		
+
 		if($modulo == 'galerias_simple' || $modulo == 'imagenes_simple' || $modulo == 'videos_simple' || $modulo == 'audios_simple') {
 			$modulo = 'galerias';
 			$temp_data = $this->caller->get_json($this->caller->construct_call($modulo, $id));
@@ -258,7 +264,7 @@ class InHouse {
 			$id = $temp_data->galerias[0]->id_galeria;
 			$modulo = 'medias';
 		}
-		
+
 		if(!isset($this->valores[$modulo][$id])) {
 			$data = $this->caller->get_json($this->caller->construct_call($modulo, $id));
 			$this->valores[$modulo][$id] = $data;
@@ -266,31 +272,31 @@ class InHouse {
 				throw new InhouseAuthException();
 			}
 		}
-		
+
 		preg_match_all('/{(\w+(\|\w*)?)}/i', $original, $vars);
-		
+
 		if(!isset($this->valores[$modulo][$id]->tipo)) {
 			throw new InhouseContentException();
 		}
-		
+
 		$tipo = $this->valores[$modulo][$id]->tipo;
 		$coleccion = '';
 		$array = $this->valores[$modulo][$id]->$modulo;
-		
+
 		$init = ($pag - 1)*$total;
-		
+
 		$total = $total * $pag;
-		
+
 		if($total == 0 || $total > count($array)) {
 			$total = count($array);
 		}
-		
+
 		for($i = $init; $i < $total; $i++) {
-			$elemento = $array[$i]; 
+			$elemento = $array[$i];
 			$contenido = $original;
 			$id = 'id_'.$tipo;
 			$this->valores[$tipo][$elemento->$id] = $elemento;
-						
+
 			foreach($vars[1] as $var) {
 				try {
 					$data = $this->getData($tipo, $var, $elemento->$id);
@@ -302,23 +308,23 @@ class InHouse {
 			}
 			$coleccion .= $contenido;
 		}
-		
+
 		return $coleccion;
 	}
-	
+
 	protected function processPaginacion($original, $identificador) {
 		list($modulo, $id) = explode('(', trim($identificador, ')'));
-				
+
 		$total = 0;
 		if(strpos($id, ',') !== false) {
 			$limit = explode(',', $id);
-			$id = $limit[0];				
+			$id = $limit[0];
 			$total = $limit[1];
 		}
-				
+
 		if((int) $id == 0) {
 			$conf = explode(':', $id);
-			
+
 			if(count($conf) == 3) {
 				$id = $this->getData($conf[0], $conf[1], $conf[2]);
 			}
@@ -326,11 +332,11 @@ class InHouse {
 				$id = isset($_GET[$id])?$_GET[$id]:0;
 			}
 		}
-				
+
 		if($modulo == 'banners' || $modulo == 'banner' || $modulo == 'videos' || $modulo == 'imagenes' || $modulo == 'audios') {
 			$modulo = 'medias';
 		}
-		
+
 		if($modulo == 'galerias_simple' || $modulo == 'imagenes_simple' || $modulo == 'videos_simple' || $modulo == 'audios_simple') {
 			$modulo = 'galerias';
 			$temp_data = $this->caller->get_json($this->caller->construct_call($modulo, $id));
@@ -343,7 +349,7 @@ class InHouse {
 			$id = $temp_data->galerias[0]->id_galeria;
 			$modulo = 'medias';
 		}
-		
+
 		if(!isset($this->valores[$modulo][$id])) {
 			$data = $this->caller->get_json($this->caller->construct_call($modulo, $id));
 			$this->valores[$modulo][$id] = $data;
@@ -351,50 +357,50 @@ class InHouse {
 				throw new InhouseAuthException();
 			}
 		}
-		
+
 		preg_match_all('/{(\w+(\|\w*)?)}/i', $original, $vars);
-		
+
 		if(!isset($this->valores[$modulo][$id]->tipo)) {
 			throw new InhouseContentException();
 		}
 
 		$coleccion = '';
 		$array = $this->valores[$modulo][$id]->$modulo;
-						
+
 		if($total != 0 && $total < count($array)) {
 			$pags = ceil(count($array)/$total);
 		}
 		else {
 			$pags = 1;
 		}
-				
+
 		for($i = 1; $i <= $pags; $i++) {
 			$contenido = str_replace('{pag}', $i, $original);
 			$contenido = str_replace('{url}', $_SERVER['REQUEST_URI'], $contenido);
 			$coleccion .= $contenido;
 		}
-		
+
 		return $coleccion;
 	}
 }
 
-class Caller 
+class Caller
 {
 	protected $dominio;
-	
+
 	protected $token;
-	
+
 	protected $id_dominio;
-		
+
 	protected $api_url = API_URL;
-	
+
 	public function __construct($id_dominio, $token)
 	{
 		$this->dominio = $_SERVER['SERVER_NAME'];
 		$this->id_dominio = $id_dominio;
 		$this->token = $token;
 	}
-	
+
 	protected function api_call($api_call)
 	{
 		$curl = curl_init($api_call);
@@ -402,15 +408,15 @@ class Caller
 		curl_setopt($curl, CURLOPT_REFERER, $this->dominio);
 		$result = curl_exec($curl);
 		curl_close($curl);
-	
+
 		if($result === false)
 		{
 			throw new Exception('Fallo la operación de CURL');
 		}
-	
+
 		return $result;
 	}
-	
+
 	public function get_json($api_call)
 	{
 		$data = json_decode($this->api_call($api_call));
@@ -434,7 +440,7 @@ class Caller
 		}
 		return $data;
 	}
-	
+
 	public function construct_call($modulo, $id) {
 		$call = $this->api_url.'?sec=api&response='.$modulo.'&token='.$this->token.'&id_dominio='.$this->id_dominio.'&dominio='.$this->dominio.'&id='.$id.'&view=json';
 		return $call;
@@ -445,7 +451,7 @@ class Caller
 $inh = new InHouse($id_dominio, $token);
 
 if(!defined('LOAD_INHOUSE')) {
-	
+
 	$request = isset($_SERVER['PATH_INFO'])?$_SERVER['PATH_INFO']:'';
 	if($request == '') {
 		$uri = isset($_SERVER['REQUEST_URI'])?$_SERVER['REQUEST_URI']:'';
@@ -454,13 +460,13 @@ if(!defined('LOAD_INHOUSE')) {
 	$request = str_replace($base_route, '', $request);
 
 	$request = trim($request, '/');
-	
+
 	$request = str_replace('.php', '.html', $request);
-	
+
 	if($request == '') {
 		$request = 'index.html';
 	}
-	
+
 	if(!file_exists($request)) {
 		header("HTTP/1.0 404 Not Found");
 		header('Status: 404 Not Found');
@@ -474,7 +480,7 @@ if(!defined('LOAD_INHOUSE')) {
 	}
 	else {
 		$contents = file_get_contents($request);
-		
+
 		try {
 			$renew_data = false;
 			$ext = pathinfo($request, PATHINFO_EXTENSION);
@@ -494,12 +500,12 @@ if(!defined('LOAD_INHOUSE')) {
 			}
 			else {
 				$contenido = $inh->parse_html($contents);
-			} 
+			}
 			if(is_writable('.')) {
 				if(!file_exists('./__cache')) {
 					mkdir('__cache', 0755);
 				}
-				
+
 				if($renew_data == true) {
 					$dir = dirname($cache);
 					if(!file_exists($dir)) {
